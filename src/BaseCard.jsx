40ab8b07c965652d914e00df7f8946c518eed0b9
Rajkumari Patidar <patidarrajkumari@gmail.com>
2024-12-08 19:34:25 +0530

    some changed

--- src/BaseCard.jsx
+++ src/BaseCard.jsx
@@ -5,7 +5,7 @@ import LogicCard from './LogicCard.jsx';
 export default  function BaseCard (){
     return(
         <div className="basediv">
-            <h2 className="baseCardH2">Let's Play Card Game</h2>
+            <h2 className="baseCardH2">Let's Play Card Game!</h2>
             <div className="cardManager">
                 <LogicCard ></LogicCard> 
             </div>
